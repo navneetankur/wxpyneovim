4a4baeda1a99bc8af86fa72f09d3abd973a81eeb
navneet aman <contact@navneetaman.com>
2020-05-11 23:36:50 +0530

    start a new neovim instance instead of trying to connect to old one

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from pynvim import attach
 import redrawevent
 import nbridge
 import ngui
-# Create a python API session attached to unix domain socket created above:
-print(r'\\.\pipe\nvim-15468-0')
 # nvim = attach('socket', path=r'\\.\pipe\nvim-15468-0')
-nvim = attach('tcp', address='127.0.0.1', port=8080)
+# subprocess.Popen(["nvim","--embed","--headless"])
+# nvim = attach('tcp', address='127.0.0.1', port=8080)
+nvim = attach('child', argv=["nvim", "--embed", "--headless"])
 nbr = nbridge.NBridge(nvim)
 # Now do some work. 
 buffer = nvim.current.buffer # Get the current buffer
@@ -13,7 +13,7 @@ buffer[0] = 'replace first line'
 nbr.input('inew line')
 nvim.vars['global_var'] = [1, 2, 3]
 gv = nvim.eval('g:global_var')
-nbr.ui_attach(400,200,True,ext_linegrid=True)
+nbr.ui_attach(30,10,True,ext_linegrid=True)
 events = redrawevent.Events(nbr)
 ui = ngui.NGUI(nbr, events, 'WxPyNeovim',800,500)
 # threading.Thread(target=events.update_loop,args=(self,)).start()
---
+++ readme.md
@@ -0,0 +1,8 @@
+A prototype neovim gui using python, pynvim and wxpython. Main.py is the starting file.
+Requirements:
+1. neovim
+2. python
+3. pynvim (pip install pynvim)
+4. wxpython (pip install wxpython)
+
+Abondened as startup time is too slow.
